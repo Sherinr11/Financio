3b352a57f9c87bbf80a133ddcde2113e721c52ee
keziamichellegeorge <144542677+keziamichellegeorge@users.noreply.github.com>
2023-09-10 18:59:37 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -7,15 +7,17 @@ import { authContext } from "@/lib/store/auth-context";
 import { currencyFormatter } from "@/lib/utils";
 
 import ExpenseCategoryItem from "@/components/ExpenseCategoryItem";
-
 import AddIncomeModal from "@/components/modals/AddIncomeModal";
 import AddExpensesModal from "@/components/modals/AddExpensesModal";
+
 import SignIn from "@/components/SignIn";
 
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import "chart.js/auto"
 import { Doughnut } from "react-chartjs-2";
 
 ChartJS.register(ArcElement, Tooltip, Legend);
+
 
 export default function Home() {
   const [showAddIncomeModal, setShowAddIncomeModal] = useState(false);
@@ -24,7 +26,8 @@ export default function Home() {
   const [balance, setBalance] = useState(0);
 
   const { expenses, income } = useContext(financeContext);
-  const { user } = useContext(authContext);
+  
+  const { user} = useContext(authContext);
 
   useEffect(() => {
     const newBalance =
@@ -75,7 +78,7 @@ export default function Home() {
             onClick={() => {
               setShowAddIncomeModal(true);
             }}
-            className="btn btn-primary-outline"
+            className="btn btn-primary-outline "
           >
             + Income
           </button>
@@ -85,9 +88,14 @@ export default function Home() {
         <section className="py-6">
           <h3 className="text-2xl">My Expenses</h3>
           <div className="flex flex-col gap-4 mt-6">
+           
             {expenses.map((expense) => {
-              return <ExpenseCategoryItem key={expense.id} expense={expense} />;
-            })}
+              return <ExpenseCategoryItem key={expense.id} expense={expense} 
+              />;
+              
+            })
+            }
+             
           </div>
         </section>
 
@@ -96,16 +104,17 @@ export default function Home() {
           <a id="stats" />
           <h3 className="text-2xl">Stats</h3>
           <div className="w-1/2 mx-auto">
+          
             <Doughnut
               data={{
                 labels: expenses.map((expense) => expense.title),
                 datasets: [
                   {
                     label: "Expenses",
-                    data: expenses.map((expense) => expense.total),
+                    data:expenses.map((expense) => expense.total),
                     backgroundColor: expenses.map((expense) => expense.color),
                     borderColor: ["#18181b"],
-                    borderWidth: 5,
+                    borderWidth: 5
                   },
                 ],
               }}
@@ -116,3 +125,5 @@ export default function Home() {
     </>
   );
 }
+
+  
